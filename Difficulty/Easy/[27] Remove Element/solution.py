fe0b751f8da7502487e3793b012728d1dc375f31
FangYang970206 <15270989505@163.com>
2019-03-30 11:00:21 +0800

    add 26, 27

---
+++ Difficulty/Easy/[27] Remove Element/solution.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+# https://leetcode.com/problems/remove-element/description/
+#
+# algorithms
+# Easy (43.64%)
+# Total Accepted:    385.8K
+# Total Submissions: 878.4K
+# Testcase Example:  '[3,2,2,3]\n3'
+#
+# Given an array nums and a value val, remove all instances of that value
+# in-place and return the new length.
+# 
+# Do not allocate extra space for another array, you must do this by modifying
+# the input array in-place with O(1) extra memory.
+# 
+# The order of elements can be changed. It doesn't matter what you leave beyond
+# the new length.
+# 
+# Example 1:
+# 
+# 
+# Given nums = [3,2,2,3], val = 3,
+# 
+# Your function should return length = 2, with the first two elements of nums
+# being 2.
+# 
+# It doesn't matter what you leave beyond the returned length.
+# 
+# 
+# Example 2:
+# 
+# 
+# Given nums = [0,1,2,2,3,0,4,2], val = 2,
+# 
+# Your function should return length = 5, with the first five elements of nums
+# containingÂ 0, 1, 3, 0, andÂ 4.
+# 
+# Note that the order of those five elements can be arbitrary.
+# 
+# It doesn't matter what values are set beyondÂ the returned length.
+# 
+# Clarification:
+# 
+# Confused why the returned value is an integer but your answer is an array?
+# 
+# Note that the input array is passed in by reference, which means modification
+# to the input array will be known to the caller as well.
+# 
+# Internally you can think of this:
+# 
+# 
+# // nums is passed in by reference. (i.e., without making a copy)
+# int len = removeElement(nums, val);
+# 
+# // any modification to nums in your function would be known by the caller.
+# // using the length returned by your function, it prints the first len
+# elements.
+# for (int i = 0; i < len; i++) {
+# print(nums[i]);
+# }
+# 
+#
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        val_index = []
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                val_index.append(i)
+            else:
+                if val_index:
+                    nums[val_index[0]] = nums[i]
+                    del val_index[0]
+                    val_index.append(i)
+                count += 1
+        return count
